03c1e34528baf18bde3ea4140ee98941584720e3
Yash Sehgal <yashsehgal.work@gmail.com>
2024-04-22 02:01:54 +0530

    wip: adding liveblocks config for testing=

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { KeySoundSelect } from "./components/key-sound-select";
 import Typearea from "./components/typearea";
+import { RoomProvider } from './liveblocks.config';
+import { ClientSideSuspense } from "@liveblocks/react";
 
 export default function App() {
   return <main className="typewriter-app-container p-12 max-md:pt-28">
     <KeySoundSelect />
     <div className="typeArea-container">
-      <Typearea />
+      <RoomProvider id="my-room" initialPresence={{}}>
+        <ClientSideSuspense fallback="Loading…">
+          {() => <Typearea />}
+        </ClientSideSuspense>
+      </RoomProvider>
     </div>
   </main>
 }
--- src/components/typearea.tsx
+++ src/components/typearea.tsx
@@ -1,6 +1,13 @@
 import { ChangeEvent, useContext } from "react";
 import { TKeySound, TypewriterContext } from "../contexts/typewriter-context";
 import { motion } from 'framer-motion';
+import LiveblocksProvider from "@liveblocks/yjs";
+import { useEffect, useMemo, useState } from "react";
+import { createEditor, Editor, Transforms } from "slate";
+import { Editable, Slate, withReact } from "slate-react";
+import { withYjs, YjsEditor } from "@slate-yjs/core";
+import * as Y from "yjs";
+import { useRoom } from "../liveblocks.config";
 
 const KeySoundPath: Record<TKeySound, string> = {
   "hard": "/sounds/key-hard.wav",
@@ -8,7 +15,33 @@ const KeySoundPath: Record<TKeySound, string> = {
 }
 
 export default function Typearea() {
+  const room = useRoom();
+  const [connected, setConnected] = useState(false);
+  const [sharedType, setSharedType] = useState<Y.XmlText>();
+  const [provider, setProvider] = useState<any>();
   const { keySound, content, setContent, isTypearea } = useContext(TypewriterContext);
+
+  // Set up Liveblocks Yjs provider
+  useEffect(() => {
+    const yDoc = new Y.Doc();
+    const yProvider = new LiveblocksProvider(room, yDoc);
+    const sharedDoc = yDoc.get("slate", Y.XmlText) as Y.XmlText;
+    yProvider.on("sync", setConnected);
+
+    setSharedType(sharedDoc);
+    setProvider(yProvider);
+
+    return () => {
+      yDoc?.destroy();
+      yProvider?.off("sync", setConnected);
+      yProvider?.destroy();
+    };
+  }, [room]);
+
+  if (!connected || !sharedType || !provider) {
+    return <div>Loading...</div>;
+  }
+
   const playTypeSound = () => {
     const soundPath: string = KeySoundPath[keySound];
     const audio = new Audio(soundPath);
@@ -25,7 +58,7 @@ export default function Typearea() {
   };
 
   return <div className="typeArea-textarea-wrapper">
-    {isTypearea && <motion.textarea
+    {/* {isTypearea && <motion.textarea
       initial={{
         opacity: 0 as number,
         y: 120 as number
@@ -39,6 +72,78 @@ export default function Typearea() {
       autoFocus
       onChange={autoResize}
       value={content}
-    />}
+    />} */}
+    {isTypearea && <SlateEditor sharedType={sharedType} />}
   </div>
+}
+
+const emptyNode = {
+  children: [{ text: "" }],
+};
+
+function SlateEditor({ sharedType }: { sharedType: Y.XmlText }) {
+
+  const { keySound, content, setContent, isTypearea } = useContext(TypewriterContext);
+  const editor = useMemo(() => {
+    const e = withReact(withYjs(createEditor(), sharedType));
+
+    // Ensure editor always has at least 1 valid child
+    const { normalizeNode } = e;
+    e.normalizeNode = (entry) => {
+      const [node] = entry;
+
+      if (!Editor.isEditor(node) || node.children.length > 0) {
+        return normalizeNode(entry);
+      }
+
+      Transforms.insertNodes(editor, emptyNode, { at: [0] });
+    };
+
+    return e;
+  }, []);
+
+  const playTypeSound = () => {
+    const soundPath: string = KeySoundPath[keySound];
+    const audio = new Audio(soundPath);
+    audio.play();
+    audio.volume = 0.3;
+  };
+
+  const autoResize = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    playTypeSound();
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    setContent(textarea.value);
+  };
+
+  useEffect(() => {
+    YjsEditor.connect(editor);
+    return () => YjsEditor.disconnect(editor);
+  }, [editor]);
+
+  return (
+    <div>
+      <div>
+        <Slate editor={editor} initialValue={[emptyNode]}>
+          {/* <Editable placeholder="Start typing here..." /> */}
+          <motion.textarea
+            initial={{
+              opacity: 0 as number,
+              y: 120 as number
+            }}
+            animate={{
+              opacity: 1 as number,
+              y: 0 as number
+            }}
+            className="outline-none focus:outline-none text-lg font-medium bg-transparent w-full h-full resize-none"
+            placeholder="Start typing your thoughts..."
+            autoFocus
+            onChange={autoResize}
+            value={content}
+          />
+        </Slate>
+      </div>
+    </div>
+  );
 }
